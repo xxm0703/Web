9aa1e2cb899ef12c63cb5cd093fcd2b718bd5260
Martin Jordanov <xxm0703@abv.bg>
2019-03-08 15:03:59 +0200

    First vue steps into Few.js

--- final_project/src/js/main.js
+++ final_project/src/js/main.js
@@ -1,4 +1,23 @@
-root = new Vue({
-    el: '#example',
-    data: { hello: 'Hello World!' }
+new Vue({
+  el: '#validation'
+  data: {
+    isInvalid: true
+  }
 })
+//
+// var vm = new Vue({
+//     el: '.container',
+//     data: { isInvalid: false,
+//     username: null,
+//     password: null,
+//     password_confirmation: null},
+//     watch: {
+//       password-confirmation: function (value) {
+//         if (value !== this.password) {
+//           isInvalid = true;
+//         } else {
+//           isInvalid = false;
+//         }
+//       }
+//     }
+// })
